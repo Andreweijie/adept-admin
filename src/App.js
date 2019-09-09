53918f1c951645e022324fc5d65d53f02e8d3407
Andre Goh <andregoh1996@gmail.com>
2019-09-09 17:50:14 +0800

    update 9/9

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import { Route, withRouter, Redirect } from "react-router-dom";
 import AuthUtils from "./components/auth/AuthUtils";
-import Login from "./components/auth/Login";
+
 import Alogin from "./components/admin/ALogin";
-import ChangePassword from "./components/auth/ChangePassword";
-import Register from "./components/auth/Register";
+
 import Reset from "./components/auth/Reset";
 import Admin from "./components/admin/Admin";
-import Cust from "./components/customers/Cust";
 import decode from "jwt-decode";
 import config from "./config";
 import "./scss/style.css";
@@ -39,21 +37,10 @@ class App extends Component {
         <Route
           exact
           path="/"
-          render={() => <Redirect to="/cust/dashboard" />}
+          render={() => <Redirect to="/admin/dashboard" />}
         />
-        <PrivateRoute path="/cust" component={Cust} />
         <Route path="/admin" component={Admin} />
         <Route exact path="/admins/login" component={Alogin} />
-        <Route
-          exact
-          path="/customer/login"
-          render={props => (
-            <Login {...props} handleStatus={this.handleStatus} />
-          )}
-        />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/reset" component={Reset} />
-        <Route exact path="/change-password" component={ChangePassword} />
       </div>
     );
   }
